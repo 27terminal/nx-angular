23985bb00e71884a9da58636cdfec8de4c640394
27terminal <27terminal@gmail.com>
2022-06-30 16:44:34 +0530

    Add books lib with ngrx

---
+++ libs/books/src/lib/books.module.ts
@@ -0,0 +1,15 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { StoreModule } from '@ngrx/store';
+import { EffectsModule } from '@ngrx/effects';
+import * as fromBooks from './+state/books.reducer';
+import { BooksEffects } from './+state/books.effects';
+
+@NgModule({
+  imports: [
+    CommonModule,
+    StoreModule.forFeature(fromBooks.BOOKS_FEATURE_KEY, fromBooks.reducer),
+    EffectsModule.forFeature([BooksEffects]),
+  ],
+})
+export class BooksModule {}
